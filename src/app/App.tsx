ec8222ed0672e7a12bc6c6574e3b4d82c767a613
Thendo Nengwenda <thendo.oneatom@gmail.com>
2026-07-01 22:15:13 +0200

    Remove GlobalPresence, HistoryTimeline, TechnologySection, and HelpSection components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,20 +2,16 @@ import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { AboutSection } from './components/AboutSection';
 import { BrandsSection } from './components/BrandsSection';
-import { GlobalPresence } from './components/GlobalPresence';
 import { ResponsibleGaming } from './components/ResponsibleGaming';
 import { InvestorSection } from './components/InvestorSection';
 import { CareersSection } from './components/CareersSection';
 import { Footer } from './components/Footer';
 import { LeadershipSection } from './components/LeadershipSection';
-import { HistoryTimeline } from './components/HistoryTimeline';
-import { TechnologySection } from './components/TechnologySection';
 import { ESGSection } from './components/ESGSection';
 import { PartnershipsSection } from './components/PartnershipsSection';
 import { NewsRoom } from './components/NewsRoom';
 import { PublishedAnnouncements } from './components/PublishedAnnouncements';
 import { ReportingSection } from './components/ReportingSection';
-import { HelpSection } from './components/HelpSection';
 import { RegulatoryPage } from './components/RegulatoryPage';
 
 // Suppple Group Corporate Website
@@ -26,10 +22,7 @@ export default function App() {
       <main>
         <Hero />
         <AboutSection />
-        <HistoryTimeline />
         <BrandsSection />
-        <GlobalPresence />
-        <TechnologySection />
         <PartnershipsSection />
         <InvestorSection />
         <LeadershipSection />
@@ -38,7 +31,6 @@ export default function App() {
         <ReportingSection />
         <ResponsibleGaming />
         <NewsRoom />
-        <HelpSection />
         <RegulatoryPage />
         <CareersSection />
       </main>
